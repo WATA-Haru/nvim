731c8170dc572083262a0ed518676ca21707fec3
hwata <hwatahiki01@gmail.com>
2023-08-23 18:26:59 +0900

    first commit

---
+++ lua/custom-keys.lua
@@ -0,0 +1,48 @@
+--fetch keymap
+local map = vim.api.nvim_set_keymap
+
+-- map the key n to run the command : NvimTreeToggle
+map('n', 'n', [[:NvimTreeToggle<CR>]], {})
+
+-- nvim-dap keymappings
+-- Press f5 to debug
+map('n', '<F5>', [[:lua require'dap'.continue()<CR>]], {})
+-- Press CTRL + b to toggle regular breakpoint
+map('n', '<C-b>', [[:lua require'dap'.toggle_breakpoint()<CR>]], {})
+-- Press CTRL + c to toggle Breakpoint with Condition
+map('n', '<C-c>', [[:lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint Condition: '))<CR>]], {})
+-- Press CTRL + l to toggle Logpoint
+map('n', '<C-l>', [[:lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log Point Msg: '))<CR>]], {})
+-- Pressing F10 to step over
+map('n', '<F10>', [[:lua require'dap'.step_over()<CR>]], {})
+-- Pressing F11 to step into
+map('n', '<F11>', [[:lua require'dap'.step_into()<CR>]], {})
+-- Pressing F12 to step out
+map('n', '<F12>', [[:lua require'dap'.step_out()<CR>]], {})
+-- Press F6 to open REPL
+map('n', '<F6>', [[:lua require'dap'.repl.open()<CR>]], {})
+-- Press dl to run last ran configuration (if you used f5 before it will re run it etc)
+map('n', 'dl', [[:lua require'dap'.run_last()<CR>]], {})
+
+-- Press Ctrl+d to toggle debug mode, will remove NvimTree also
+map('n', '<C-d>', [[:NvimTreeToggle<CR> :lua require'dapui'.toggle()<CR>]], {})
+
+-- update leader key to space
+vim.g.mapleader = " "
+
+
+-- use keymap in file-header-view(bufferline-plugin)
+-- bufferline close setting
+vim.keymap.set('n', '<leader>wl', '<CMD>BufferLineCloseRight<CR>')
+vim.keymap.set('n', '<leader>wh', '<CMD>BufferLineCloseLeft<CR>')
+vim.keymap.set('n', '<leader>wall', '<CMD>BufferLineCloseOthers<CR>')
+vim.keymap.set('n', '<leader>we', '<CMD>BufferLinePickClose<CR>')
+
+-- (reference)https://github.com/kazhala/dotfiles/blob/master/.config/nvim/lua/kaz/plugins/bufferline.lua
+vim.keymap.set('n', 'gb', '<CMD>BufferLinePick<CR>')
+vim.keymap.set('n', '<leader>ts', '<CMD>BufferLinePickClose<CR>')
+vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
+vim.keymap.set('n', ']b', '<CMD>BufferLineMoveNext<CR>')
+vim.keymap.set('n', '[b', '<CMD>BufferLineMovePrev<CR>')
+vim.keymap.set('n', 'gs', '<CMD>BufferLineSortByDirectory<CR>')
