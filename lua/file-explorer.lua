57a55ef01387379d8218a334d71f9031148fed9a
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2023-08-24 23:48:00 +0900

    mod and divide init.lua

--- lua/file-explorer.lua
+++ lua/file-explorer.lua
@@ -49,3 +49,10 @@ require("nvim-tree").setup
 
 	on_attach = 'default'
 }
+
+-- automated settings
+local function open_nvim_tree()
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
