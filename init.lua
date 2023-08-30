cc68f5943acabf6b05ef5b8fd076f26864b9de3b
hwata <hwatahiki01@gmail.com>
2023-08-31 00:20:15 +0900

    replace packer to lazy

--- init.lua
+++ init.lua
@@ -1,42 +1,31 @@
--- Plugins
-require('plugins')
-require('custom-keys')
-require('mason-config')
-require('file-explorer')
-require('file-finder')
-require('file-header-view')
-require('code-completion')
-require('debugging')
-require('styling')
-require('syntax-highlight')
-require('statusbar')
-require('commentout')
+require('common-setting')
+require('keys')
+require('lazynvim')
 
--- set number
-vim.opt.number = true
+-- plugins
+--require('plugins')
+--require('mason-config')
+--require('file-explorer')
+--require('file-finder')
+--require('file-header-view')
+--require('code-completion')
+--require('debugging')
+--require('styling')
+--require('syntax-highlight')
+--require('statusbar')
+--require('commentout')
+--require('night-fox')
 
--- show terminal title
-vim.opt.title = true
-
-vim.opt.encoding = "utf-8"
-
--- vim.opt.tabstop = 4
-
--- show tab as  ^ charcter
-vim.opt.list = false
-vim.opt.clipboard = "unnamedplus"
-
--- ESC to jk
-vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true })
+-- vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true })
 
 -- leader key to space
-vim.g.mapleader = " "
-vim.cmd("filetype plugin indent on")
+-- vim.g.mapleader = " "
+-- vim.cmd("filetype plugin indent on")
 
 -- spell check
-vim.opt.spelllang = 'en_us'
-vim.api.nvim_set_keymap('n', '<leader>f', '1z=', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>s', ':set spell!<CR>', { noremap = true })
+--vim.opt.spelllang = 'en_us'
+--vim.api.nvim_set_keymap('n', '<leader>f', '1z=', { noremap = true })
+--vim.api.nvim_set_keymap('n', '<leader>s', ':set spell!<CR>', { noremap = true })
 
 -- windows wsl only settings 
 -- but if you write if vim.fn.has("win64") == 1 then , system cant use clipboard.
---
+++ lua/common-setting.lua
@@ -0,0 +1,14 @@
+-- set number
+vim.opt.number = true
+
+-- show terminal title
+vim.opt.title = true
+
+vim.opt.encoding = "utf-8"
+
+-- vim.opt.tabstop = 4
+
+-- show tab as  ^ charcter
+vim.opt.list = false
+vim.opt.clipboard = "unnamedplus"
+
