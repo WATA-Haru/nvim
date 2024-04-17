08f2bb55e7aaac70b02379ab9f93ad2c663d9fc0
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-04-17 18:49:13 +0900

    fix lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
 	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	--dependencies = { 'nvim-tree/nvim-web-devicons' },
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 	require('lualine').setup {
