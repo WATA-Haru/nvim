e3442d578cb771a71865357b6e0b19915ee3edfc
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-13 22:51:44 +0900

    [FIX] mason lsp config tsserver to ts_ls

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -23,8 +23,8 @@ return {
         "docker_compose_language_service",
         "rust_analyzer",
         "volar",
-        --"ts_ls",
-        "tsserver",
+        --"tsserver",
+        "ts_ls",
 				"html",
 				"cssls",
 				"hls",
@@ -58,7 +58,7 @@ return {
     }
 
 		local vue_typescript_plugin = require("mason-registry").get_package("vue-language-server"):get_install_path() .. "/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
-		lspconfig.tsserver.setup({
+		lspconfig.ts_ls.setup({
 			filetypes = {
 				"javascript",
 				"javascriptreact",
@@ -78,49 +78,5 @@ return {
 			},
 		})
 		lspconfig.volar.setup({})
-		-- vue with ts setting (https://minerva.mamansoft.net/Notes/%F0%9F%93%9Dnvim-lspconfig%E3%81%A7Vue%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%82%92%E9%96%8B%E3%81%84%E3%81%A6%E3%82%82Volar%E3%81%8C%E6%A9%9F%E8%83%BD%E3%81%97%E3%81%AA%E3%81%84)
- --   local lspconfig = lspconfig
- --   require("lspconfig.ui.windows").default_options.border = "single"
-
- --   local capabilities = require("cmp_nvim_lsp").default_capabilities()
- --   capabilities.textDocument.completion.completionItem.snippetSupport = true
-
- --   local home = os.getenv("HOME")
- --   local function organize_imports()
- --     local params = {
- --       command = "_typescript.organizeImports",
- --       arguments = { vim.api.nvim_buf_get_name(0) },
- --       title = "",
- --     }
- --     vim.lsp.buf.execute_command(params)
- --   end
-
- --   lspconfig.volar.setup({
- --     capabilities = capabilities,
- --   })
- --   lspconfig.tsserver.setup({
- --     capabilities = capabilities,
- --     single_file_support = false,
- --     init_options = {
- --       plugins = {
- --         {
- --           name = "@vue/typescript-plugin",
- --           location = home
- --             .. "/.local/share/mise/installs/node/20/lib/node_modules/@vue/language-server",
- --           languages = { "javascript", "typescript", "vue" },
- --         },
- --       },
- --     },
- --     commands = {
- --       OrganizeImports = {
- --         organize_imports,
- --       },
- --     },
- --     filetypes = {
- --       "javascript",
- --       "typescript",
- --       "vue",
- --     },
- --   })
   end,
 }
